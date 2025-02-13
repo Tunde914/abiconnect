aa395324c4e0cf4eed800f6fd3e819ca00228a44
Tunde914 <ishautunde700@gmail.com>
2025-02-13 21:57:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars");
+});
+
+navLinks.addEventListener("click", (e) => {
+    if (e.target.tagName === "A" && window.innerWidth <= 768) { 
+        navLinks.classList.remove("open");
+        menuBtnIcon.setAttribute("class", "fa-solid fa-bars");
+    }
+});
+
+
+const scrollRevealOption = {
+    origin: "bottom",
+    distance: "50px",
+    duration: 1000,
+    easing: "ease",
+};
+
+ScrollReveal().reveal(".intro-img img", {
+    ...scrollRevealOption,
+    origin:"right",
+});
+ScrollReveal().reveal(".intro-text h1", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+ScrollReveal().reveal(".btn-2", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+ScrollReveal().reveal(".text", {
+    ...scrollRevealOption,
+    delay: 1000,
+}); 
+
+ScrollReveal().reveal(".text-2", {
+    ...scrollRevealOption,
+    delay: 1000,
+}); 
+ScrollReveal().reveal(".service-card", {
+    ...scrollRevealOption,
+    interval: 500,
+});
+
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    let userEmail = document.getElementById("userEmail").value;
+    
+    if (userEmail) {
+        // Open email client with pre-filled email
+        window.location.href = `mailto:abiconnectdigitaledu@gmail.com?subject=New Contact Request&body=User Email: ${userEmail}`;
+    } else {
+        alert("Please enter your email address before proceeding.");
+    }
+});
